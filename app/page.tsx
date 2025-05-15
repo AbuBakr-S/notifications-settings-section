05333290af7b246dc6b6e0f6107e97f77bbbcf69
AbuBakr-S <abz.siddique@gmail.com>
2025-05-15 17:56:41 +0100

    feat: add mobile design

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { Noto_Sans } from "next/font/google";
+
+const notoSans = Noto_Sans({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+  variable: "--font-noto",
+});
+
 const NOTIFICATIONS = [
   { id: "marketing", label: "Marketing & promotional content" },
   { id: "features", label: "Feature updates" },
@@ -10,21 +18,22 @@ const CHANNELS = ["Push", "Email", "SMS"];
 
 export default function Home() {
   return (
-    <div>
+    <div className={`${notoSans.className} px-4 mt-16`}>
       <div>
-        <h1>Manage Your Notifications</h1>
-        <p>
+        <h1 className="text-xl font-semibold">Manage Your Notifications</h1>
+        <p className="text-sm font-normal text-neutral-500 mt-2">
           Choose how you want to be notified about the latest updates and
           messages.
         </p>
         <form>
-          <div>
-            <table>
+          <div className="mb-6">
+            <table className="mt-8 w-full text-sm">
               <thead>
                 <tr>
                   <th></th>
                   {CHANNELS.map((channel) => (
                     <th
+                      className="font-semibold px-3 py-2"
                       key={channel}
                     >
                       {channel}
@@ -34,11 +43,11 @@ export default function Home() {
               </thead>
               <tbody>
                 {NOTIFICATIONS.map(({ id, label }) => (
-                  <tr key={id}>
-                    <td>{label}</td>
+                  <tr key={id} className="first:border-t border-neutral-200">
+                    <td className="max-w-36">{label}</td>
                     {CHANNELS.map((channel) => (
                       // TODO: Replace with a Reusable Toggle Component
-                      <td key={channel}>
+                      <td key={channel} className="text-center">
                         Toggle
                       </td>
                     ))}
@@ -48,8 +57,9 @@ export default function Home() {
             </table>
           </div>
 
-          <div>
+          <div className="flex justify-end items-center h-[80px]">
             <button
+              className="w-[176px] h-[48px] bg-neutral-100"
               type="submit"
             >
               Save Changes
